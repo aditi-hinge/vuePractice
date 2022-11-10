6086b1fa9937bbfb81ae84b36a309cf5f4e12bbb
aditi-hinge <aditi.hinge@gmail.com>
2022-11-10 21:18:12 +0900

    Creating a Vue app

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello vue")
+//to create an app
+const app = Vue.createApp()
+
+//tells the app at what point or
+//where to mount the application
+//here, it tells vue to take our app
+//and mount it in the DOM where the 
+//element with an id 'app' is present
+app.mount('#app')
