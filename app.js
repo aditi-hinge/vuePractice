8bd3783d76cb300646582ac489809132e2d08aff
aditi-hinge <aditi.hinge@gmail.com>
2022-11-12 11:22:14 +0900

    Inserting values dynamically

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ const app = Vue.createApp({
     // template: '<h2>I am a template</h2>'
     //but because creating templates here can get messy,
     //it is better to add the template directly
+
+    //a variable can be created to show the value dynamically
+    //by creating a data function
+    //shorthand for a function inside an object
+    //syntax- function name, brackets and paranthesis
+    data() {
+        //inside the function a data object can be returned
+        //therefore, function must be created and must be returned inside it
+        return {
+            title: '1984',
+            author: 'George Orwell',
+            publicationYear: 1949
+        }
+    }
 })
 
 //tells the app at what point or
